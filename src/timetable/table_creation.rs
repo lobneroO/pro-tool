acc3bdc7e431c90d0509a524d9db0b07e0506d56
lobneroO <dev.tl@mailbox.org>
2024-07-14 10:45:59 +0200

    added actual time for y axis. labels not correct. added a rectangle drawing in upper left corner for testing.

--- src/timetable/table_creation.rs
+++ src/timetable/table_creation.rs
@@ -1,8 +1,14 @@
 // Tim Lobner
 
 use std::path::Path;
-use plotters::prelude::{SVGBackend, ChartBuilder, IntoDrawingArea, LabelAreaPosition, IntoSegmentedCoord, LineSeries, BLACK, WHITE};
+// use plotters::prelude::{SVGBackend, ChartBuilder, IntoDrawingArea, LabelAreaPosition, IntoSegmentedCoord, DrawingBackend, Rectangle};
+// use plotters::prelude::{BLACK, WHITE};
+// TODO: use above individual use pattern...
+use plotters::prelude::*;
 
+use chrono::NaiveDate;
+
+use crate::band::Band;
 
 pub fn create_table(out_path: &Path){
     println!("writing image to {}", out_path.display());
@@ -11,10 +17,35 @@ pub fn create_table(out_path: &Path){
     let days = [ "Wednesday", "Thursday", "Friday", "Saturday" ];
     let current_day_index = 2;
     let current_day = days[current_day_index];
-    let num_stages = 4;
+    let stages = [ "Main Stage", "T Stage", "Wera Rebel Stage", "Campsite Circus Stage" ];
 
-    let first_band_start = 12;
-    let last_band_end = 27;
+    // TODO: naive date time not supported by plotter library
+    // but UTC is. converting everything utc on the fly should work,
+    // since the user will not care if the output is 8am local or 8utc
+    let first_band_start = 
+        NaiveDate::from_ymd_opt(2024, 8, 15)
+            .unwrap()
+            .and_hms_opt(6, 0, 0).
+            unwrap().and_utc();
+    let last_band_end = 
+        NaiveDate::from_ymd_opt(2024, 8, 15)
+            .unwrap()
+            .and_hms_opt(11, 59, 0).
+            unwrap().and_utc();
+
+    let fleshgod = Band {
+        name: String::from("Fleshgod Apocalypse"),
+        selected: true,
+        stage: String::from("T Stage"),
+        start_dt: NaiveDate::from_ymd_opt(2024, 8, 15).unwrap().and_hms_opt(17, 20, 0).unwrap(), 
+        end_dt: NaiveDate::from_ymd_opt(2024, 8, 15).unwrap().and_hms_opt(18, 30, 0).unwrap(),
+    };
+
+    let d1 = NaiveDate::from_ymd_opt(2024, 8, 15).unwrap().and_hms_opt(17, 20, 0).unwrap().and_utc(); 
+    let d2 = NaiveDate::from_ymd_opt(2024, 8, 15).unwrap().and_hms_opt(18, 30, 0).unwrap().and_utc(); 
+
+    let data = [d1, d2];
+    let bands = [ fleshgod ];
 
     // use an SVG backend to have selectable text.
     // turn into a pdf for printing later
@@ -24,7 +55,7 @@ pub fn create_table(out_path: &Path){
     let mut chart = ChartBuilder::on(&drawing_area)
         .caption(current_day, ("Arial", 30))
         .set_label_area_size(LabelAreaPosition::Left, 40)
-        .set_label_area_size(LabelAreaPosition::Bottom, 40)
+        .set_label_area_size(LabelAreaPosition::Bottom, 60)
         // the x axis are the sages. we want the names centered
         // on an entry (i.e. it starts with half in front of a segment
         // and ends half after a segment, rather than matching segments 
@@ -32,7 +63,7 @@ pub fn create_table(out_path: &Path){
         // ----|---------------|-------------|-------
         //   main stage     t stage      wera stage 
         // for that behaviour, we need the into_segmented() command
-        .build_cartesian_2d((0..num_stages).into_segmented(), last_band_end..first_band_start)
+        .build_cartesian_2d(stages.into_segmented(), first_band_start..last_band_end)
         .unwrap();
 
     // draw axes labels for time on y axis (top to bottom for increasing time)
@@ -42,6 +73,12 @@ pub fn create_table(out_path: &Path){
     chart.configure_mesh()
         .disable_x_mesh()
         .draw().unwrap();
+
+    // draw based on data
+    drawing_area.draw(&Rectangle::new(
+        [(0, 0), (100, 100,)], // coordinates
+        Into::<ShapeStyle>::into(BLACK).filled(),
+    )).unwrap();
 
     // draw a diagonal line
     // chart.draw_series(
