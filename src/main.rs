b8ade5d045a6162f49df056b2a6f899b3e658d4e
tim <lobner@synopsys.com>
2025-06-18 18:43:14 +0200

    set up a quicker test way by just plotting something hardcoded for now

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,21 @@ mod timetable{
 }
 
 use gui::pro_tool_state::ProToolState;
+use plotters::prelude::*;
 
-fn main() -> iced::Result{
-    ProToolState::run(Settings::default())
+fn draw_test() {
+    let mut backend = SVGBackend::new("test.svg", (1024, 768));
+    _ = backend.draw_rect((10, 10), (25, 50), &RED, true);
+    _ = backend.draw_rect((25, 10), (40, 50), &GREEN, true);
+    let text_style = &("sans-serif", 20).into_text_style(&backend.get_size()).color(&BLACK);
+    _ = backend.draw_text("Fleshgod\n Apocalypse", text_style, (25, 10));
+    _ = backend.present();
 }
+
+fn main() {
+    draw_test();
+}
+// fn main() -> iced::Result{
+//     iced::application("Pro Tool", ProToolState::update, ProToolState::view)
+//         .run()
+// }
